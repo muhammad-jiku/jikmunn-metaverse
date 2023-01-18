3d21a299dba5c3a3f78ea9ddccb6bceefd9dab25
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-18 13:55:01 +0600

    css file moved

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import Footer from '@/src/components/Footer/Footer';
 import Navbar from '@/src/components/Navbar/Navbar';
-import './globals.css';
+import '../src/styles/globals.css';
 
 export default function RootLayout({ children }) {
   return (
