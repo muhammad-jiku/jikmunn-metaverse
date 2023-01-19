e978869ce41e3799631544621b8dbe902119f2ad
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-19 14:12:15 +0600

    homepage modified

--- app/page.jsx
+++ app/page.jsx
@@ -15,13 +15,30 @@ const Home = () => {
     //   <Navbar />
     <div>
       <Hero />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+        <WhatsNew />
+      </div>
+      <World />
+      <div className="relative">
+        <Insights />
+        <div className="gradient-04 z-0" />
+        <Feedback />
+      </div>
+      {/* <Hero />
       <About />
       <Explore />
       <GetStarted />
       <WhatsNew />
       <World />
       <Insights />
-      <Feedback />
+      <Feedback /> */}
     </div>
     // </div>
   );
